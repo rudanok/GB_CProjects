42efa0602e11bba970ee02c9889d08bd437c5fe9
rudanok <a.rudanok@gmail.com>
2022-08-28 20:15:38 +0300

    Решение задачи 8

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число:");
+int number1 = 0;
+bool check1 = int.TryParse(Console.ReadLine()!, out number1);
+
+if (check1)
+{
+    Console.Write("Чётные числа: ");
+    for (int i = 1; i <= number1; i++)
+    {
+        if (i % 2 == 0) Console.Write($"{i}, ");    
+    }
+}
+else
+{
+    Console.Write("Некорректный ввод данных!"); 
+}
