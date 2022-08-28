96cf9520cb0911d846cfcce49787d9f438a621ba
rudanok <a.rudanok@gmail.com>
2022-08-28 20:07:05 +0300

    Решение задачи 6

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Введите число:");
+double number1 = 0;
+bool check1 = false;
+check1 = double.TryParse(Console.ReadLine()!, out number1);
+
+if (check1)
+{
+    if (number1 % 2 == 0) Console.Write($"Число {number1} является чётным.");
+    else Console.Write($"Число {number1} является нечётным.");   
+}
+else
+{
+    Console.Write("Некорректный ввод данных!"); 
+}
